44641b2f822f707905e1778aa29ef30ff296fa0f
Faizan <faizanswabi95@gmail.com>
2024-03-24 20:08:24 +0500

    initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,21 +33,21 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_2rfr178',
+        'template_w5fmu7u',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Faizan",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "faizanswabi95@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'rM0NYyyG7TUoHyPmw'
       )
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you for reaching out! Your message has been sent successfully. I'll get back to you as soon as possible.");
 
           setForm({
             name: "",
